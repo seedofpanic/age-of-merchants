9f8fb352b7f96b85b7abcc3977fd9748d9798dcd
seedofpanic <seedofpanic@mail.ru>
2014-09-09 00:15:15 +0400

    top menu

--- application/views/template.php
+++ application/views/template.php
@@ -3,15 +3,37 @@
 		<title>Age of Merchants</title>
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/style.css">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
+		<div class="bs-component">
+              <div class="navbar navbar-default">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Age of Merchants</a>
+                </div>
+                <div class="navbar-collapse collapse navbar-responsive-collapse">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="aom.mobmind.ru">Main</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right quick-login">
+                    <li><input type="text" class="form-control" placeholder="login"/></li>
+                    <li><input type="password" class="form-control" placeholder="password"/></li>
+                    <li><div class="btn btn-default">Log in</div></li>
+                  </ul>
+                </div>
+              </div>
+            <div id="source-button" class="btn btn-primary btn-xs" style="display: none;">&lt; &gt;</div>
+        </div>
 		<?=$content?>
 	</body>
 </html>
