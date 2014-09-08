cc68bdf6530d8edac9abd75b2a504d77c1161382
seedofpanic <seedofpanic@mail.ru>
2014-09-08 22:07:15 +0400

    Welcome controller renamed to Main

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -137,6 +137,6 @@ Kohana::modules(array(
  */
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'controller' => 'welcome',
+		'controller' => 'main',
 		'action'     => 'index',
 	));
--- application/classes/Controller/Main.php
+++ application/classes/Controller/Main.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Main extends Controller {
 
 	public function action_index()
 	{
