31643b0e6e7cd8608c350e5078b75c28c7beb9f0
seedofpanic <seedofpanic@mail.ru>
2014-09-13 14:26:12 +0400

    Class names hotfix

--- application/classes/Model/Map/Field.php
+++ application/classes/Model/Map/Field.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class Model_Map_Field extends ORM {
+class Model_Map_field extends ORM {
 	
 	protected $_belongs_to = array('region' => array('model' => 'map_region', 'foreign_key' => 'region_id'));
 
--- application/classes/Model/Map/Region.php
+++ application/classes/Model/Map/Region.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class Model_Map_Region extends ORM {
+class Model_Map_region extends ORM {
 
     protected $_has_many = array('fields' => array('model' => 'map_field', 'foreign_key' => 'region_id'));
 
