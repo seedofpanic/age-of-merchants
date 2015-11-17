116eaacc0a07e87029432b254f4ed7a6565ae6f4
seedofpanic <seedofpanic@mail.ru>
2015-11-17 13:07:33 +0300

    Gold mine fix

--- test.js
+++
@@ -1,8 +0,0 @@
-var models = require('./models/index.js');
-models.profiles.find({where: {id: 1}}).then(function (profile) {
-   profile.save().then(function (profile) {
-       console.log('ok')
-   }, function (err) {
-       console.log(err)
-   })
-});
