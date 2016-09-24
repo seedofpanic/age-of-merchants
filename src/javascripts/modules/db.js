3386a4709851414b6360e0a1fa58e4d8a5174183
seedofpanic <seedofpanic@mail.ru>
2016-09-24 15:28:46 +0300

    #22 db.coffee

---
+++ src/javascripts/modules/db.js
@@ -0,0 +1,62 @@
+angular.module('DB', [])
+.factory('Regions', Regions)
+.factory('ProfileBuildings', ProfileBuildings)
+.factory('Profile', Profile);
+
+Regions.$inject = ['$http'];
+
+function Regions($http) {
+  var regions = {}
+  $http.get('/api/regions').then(function (res) {
+    $.each(res.data, function () {
+      regions[this.id] = this;
+    });
+  });
+  return regions;
+}
+
+ProfileBuildings.$inject = ['$http', '$route'];
+
+function ProfileBuildings($http, $route) {
+  var buildings = {arr: {}};
+  buildings.get = function () {
+    var profile_id = $route.current.params.profile_id;
+    buildings.arr = {};
+    $http.get('/api/buildings?profile_id=' + profile_id).then(function (res) {
+      $.each(res.data, function () {
+        buildings.arr[this.id] = this;
+      });
+      buildings.length = res.data.length;
+    });
+    buildings.push = function (building) {
+      buildings.arr[building.id] = building;
+      buildings.length++;
+    };
+
+    return buildings;
+  }
+}
+
+Profile.$inject = ['$http', '$route', '$rootScope'];
+
+function Profile($http, $route, $rootScope) {
+  var profile = {}
+  var get = function (profile_id) {
+    if (profile_id) {
+      $http.get('/api/profile?id=' + profile_id)
+          .then(function (res) {
+                angular.copy(res.data, profile);
+                profile.update = function () {
+                  get($route.current.params.profile_id);
+                };
+              }
+          )
+    }
+  }
+  get($route.current.params.profile_id);
+
+  $rootScope.$on('$routeChangeSuccess', function (scope, next, current) {
+    get(next.params.profile_id);
+  })
+  return profile;
+}
